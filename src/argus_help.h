d53cfceab0ee1debfbb6a86e54efe4d5230fc050
Chris Helmich <kagekirin@gmail.com>
2022-01-21 20:46:32 +0900

    Add extern "C" {} namespace around function declarations for improved C++ compatibility

--- src/argus_help.h
+++ src/argus_help.h
@@ -1,6 +1,15 @@
 #ifndef ARGUS_HELP_H_INC
 #define ARGUS_HELP_H_INC
 
-int argus_Help(int, char**);
+#ifdef __cplusplus
+extern "C"
+{
+#endif  // __cplusplus
+
+    int argus_Help(int, char**);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
 
 #endif  // ARGUS_HELP_H_INC
